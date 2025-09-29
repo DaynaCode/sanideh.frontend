553620dbbe6c36d12eb82fd7d56351350f018f8e
mohammadrezaAhmadzadeh <130323047+mrahmadzadeh@users.noreply.github.com>
2025-09-29 19:12:55 +0330

    create and complete contact us page without full responsive

--- src/app/en/contact-us/page.jsx
+++ src/app/en/contact-us/page.jsx
@@ -1,11 +1,14 @@
-
+import ContactUs from "@/components/templates/contactUs/ContactUs";
 import Header from "@/components/templates/header/Header";
 import { language } from "@/utils/dataContainer/language/language";
 import React from "react";
 function page() {
+  const lang = language.en;
+  const route = 3;
   return (
     <>
-      <Header lang={language.en} route={3} />
+      <Header lang={lang} route={route} />
+      <ContactUs lang={lang} />
     </>
   );
 }
